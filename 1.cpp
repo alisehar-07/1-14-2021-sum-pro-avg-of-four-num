978faa93166611b7ec2b9427cf2073a284c0f25a
Ali Sehar <77107640+alisehar-07@users.noreply.github.com>
2021-01-14 16:34:25 +0500

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>;
+using namespace std;
+int main()
+{ 
+    float a,b,c,d;
+	cout<<"Enter the 1st num=";
+	cin>>a;
+	cout<<"Enter the 2nd num=";
+	cin>>b;
+	cout<<"Enter the 3rd num=";
+	cin>>c;
+	cout<<"Enter the 4th num=";
+	cin>>d;
+	cout<<"The sum of numbers="<<a+b+c+d<<endl;
+	cout<<"The pro of numbers="<<a*b*c*d<<endl;
+	cout<<"Avg of numbers="<<(a+b+c+d)/4;
+}
